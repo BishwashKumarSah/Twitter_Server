995d51f00d34066445161202de6775eb5cb48266
Bishwash Kumar Sah <bishwash.sah2121@gmail.com>
2025-01-13 22:34:22 +0530

    create tweet mutation

--- src/app/index.ts
+++ src/app/index.ts
@@ -16,7 +16,7 @@ export async function initServer() {
   app.use(cors());
   app.use(bodyParser.json());
 
-  app.use("/health", (req, res) =>
+  app.get("/health", (req, res) =>
     res.status(200).json({ message: "Health is Good!" })
   );
 
